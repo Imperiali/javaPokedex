35bbef85fecc40ce38739d424528c17f9e72e039
Igor Imperiali <igor.fernandes@al.infnet.edu.br>
2019-05-22 09:07:47 -0300

    Start Pokemon class

---
+++ src/com/company/Pokemon/Pokemon.java
@@ -0,0 +1,11 @@
+package com.company.Pokemon;
+
+public abstract class Pokemon {
+  protected String name;
+  protected String type;
+  protected boolean shine;
+  protected int level;
+  protected int exp;
+  protected String[] skills;
+
+}
